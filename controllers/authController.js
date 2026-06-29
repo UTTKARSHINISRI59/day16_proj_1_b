487d1d703748d31cb7681975673eed9202991f6b
UTTKARSHINISRI59 <uttkarshinisrivastva@gmail.com>
2026-06-29 02:31:21 -0400

    ff

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,19 @@ const User=requrie ("../models/User");
 const jwt=require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-const register=()=>{
+const register=async()=>{
     try{
+        const {name,email,password}=req.body;
+        const oldUser=await User.findOne({email});
+        if(oldUser){
+            return res.status(400).json({
+                success:false,
+                message:"user already exist"
+            });
+        }
+        const hashPassword = await bcrypt.hash(password,10);
+        const user= await User.create({name,email,password:hashPassword});
+        res.status(201)
 
     }
     catch(err){
@@ -15,7 +26,12 @@ const register=()=>{
     }
 };
 
-const login=()=>{}
+const login=()=>{
+    try{
+        const {name,email,password}=requestAnimationFrame.body;
+        const oldUser= 
+    }
+}
 
 const profile=()=>{};
 
